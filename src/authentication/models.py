07234c3040db56f8e23ea6863c1eca2b33bd1630
Cândido Júnior <candidonogueirajr@gmail.com>
2024-12-15 21:17:53 -0300

    refactor: add profile roles

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -3,4 +3,17 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    class Role(models.TextChoices):
+        ADMIN = "admin", "Admin"
+        CUSTOMER = "customer", "Customer"
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.CUSTOMER)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+        unique_together = ("user", "role")
+
+    def __str__(self):
+        return self.user.username
